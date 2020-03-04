6304d4859d82fc474bb0ea117750b2a6177c70f6
pyrokuna <31514534+pyrokuna@users.noreply.github.com>
2020-03-04 01:03:20 -0500

    Finished Insertion Sort and Progress on Bucket
    I finished insertion sort although I need to test it on linprog with larger sample sizes. I was working on bucket but needed clarity on a couple of things regarding our permission in using vectors and the type of input.

---
+++ Programming_Assignment_1/1_insertion.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void printArr(int arr[], int size) {
+    printf("< ");
+    int i = 0;
+    for (i; i < size; i++) {
+        printf("%d ",arr[i]);
+    }
+    printf(">\n");
+}
+
+void insertionSort(int arr[], int size) {
+    int i, j = 1, key;
+    for (j; j < size; j++) {
+        key = arr[j];
+        i = j - 1;
+        while (i >= 0 && arr[i] > key) {
+            arr[i+1] = arr[i];
+            i = i - 1;
+        }
+        arr[i + 1] = key;
+        printArr(arr, size);
+    }
+}
+
+int main() {
+    int temp[] = {10, 11, 9, 0, 5, 8, 13};
+
+    printArr(temp, sizeof(temp)/sizeof(temp[0]));
+    insertionSort(temp, sizeof(temp)/sizeof(temp[0]));
+
+    return 0;
+}
--- Programming_Assignment_1/7_bucket.c
+++ Programming_Assignment_1/7_bucket.c
@@ -1,5 +1,27 @@
+#include <stdio.h>
+
+void bucketSort(int arr[], int n) {
+
+    // will likely have to do this in c++ if I have to use vectors
+    // check to make sure that the input will be U[0,1) real numbers (if it is, then a bucket of size 10 makes sense)
+    vector<float> bucket[10];
+}
+
+void printArr(int arr[], int size) {
+    printf("< ");
+    int i = 0;
+    for (i; i < size; i++) {
+        printf("%d ",arr[i]);
+    }
+    printf(">\n");
+}
 
 int main() {
-    printf("blah");
+    int temp[] = {10, 11, 9, 0, 5, 8, 13};
+
+    printArr(temp, sizeof(temp)/sizeof(temp[0]));
+    bucketSort(temp, sizeof(temp)/sizeof(temp[0]));
+    printArr(temp, sizeof(temp)/sizeof(temp[0]));
+
     return 0;
 }
